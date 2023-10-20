1562df8c13a0d85154b2317f5cf5628783b3d24c
Matthew Pyle <matthew.pyle@noaa.gov>
2023-10-20 19:10:22 +0000

    A bit more to help get the NAM running under rocoto.  Keeping some things as local changes, though (JNAM_FOREAST_exports, for example).

--- rocoto/run_NAM.sh
+++ rocoto/run_NAM.sh
@@ -10,11 +10,17 @@ module load intel/${intel_ver}
 module load craype/${craype_ver}
 module load cray-mpich/${cray_mpich_ver}
 module load cray-pals/${cray_pals_ver}
+module load libjpeg/${libjpeg_ver}
+module load grib_util/${grib_util_ver}
 
 module use /apps/ops/test/nco/modulefiles/
 module load core/rocoto/1.3.5
 
+module list
+
 mkdir -p /lfs/h2/emc/ptmp/Matthew.Pyle/output_para
 mkdir -p /lfs/h2/emc/stmp/Matthew.Pyle/tmp
 
-rocotorun -v 10 -w /lfs/h2/emc/lam/noscrub/Matthew.Pyle/nam.v4.2.5/rocoto/NAM_wcoss2_ops.xml -d /lfs/h2/emc/lam/noscrub/Matthew.Pyle/nam.v4.2.5/rocoto/NAM_wcoss2_ops.db
+rocotorun -v 10 \
+        -w /lfs/h2/emc/lam/noscrub/Matthew.Pyle/nam.v4.2.5/rocoto/NAM_wcoss2_ops.xml \
+	-d /lfs/h2/emc/lam/noscrub/Matthew.Pyle/nam.v4.2.5/rocoto/NAM_wcoss2_ops.db
